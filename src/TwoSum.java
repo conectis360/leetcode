415139e4dafd82516e75d6cb8d10fea2dc22dbd7
James Garcia <conectis360@gmail.com>
2025-03-13 22:35:43 -0300

    fix: comentários

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -4,19 +4,21 @@ import java.util.Map;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
+        //está usando força bruta ao invés de uma solução inteligente, itera todo o array 2x sem necessidade.
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j}; // Retorna os índices, não os valores
+                    return new int[]{i, j}; // retorna os índices, não os valores
                 }
             }
         }
-        return new int[0]; // Retorna um array vazio se nenhum par for encontrado
+        return new int[0]; // retorna um array vazio se nenhum par for encontrado
     }
 
     public int[] twoSumFaster(int[] nums, int target) {
         // é mais rápido pois processa apenas uma unica vez ao invés de 2 e também não percorre todo o array
         Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
@@ -24,6 +26,7 @@ public class TwoSum {
             }
             map.put(nums[i], i);
         }
+        
         return new int[0];
     }
 
